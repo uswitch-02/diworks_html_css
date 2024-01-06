0055f26827ff106375740eb67ddb86ca659e1010
uswitch-02 <chunxidubian64@gmail.com>
2024-01-06 05:44:51 +0000

    [update]

--- javascript/script.js
+++ javascript/script.js
@@ -586,20 +586,67 @@ document.write("Good");
 document.write("090-111-2222");
 document.write("電話番号");
 
+var a = "山本";
+document.write(a);
 
+var number = 208;
+document.write(number);
 
+var tell = "090-0000-1234";
+document.write(tell);
 
+var name ="jody";
+document.write(name);
 
+var name2="高橋さんと田中さん";
+document.write(name2);
 
+var cost ="2001円";
+document.write(cost);
 
+var month = 12;
+document.write(month + "月");
 
+var day = 25;
+document.write(day + "日");
 
+var youbi ="木";
+document.write(youbi + "曜日");
 
+document.write(3*3);
+document.write(4/2);
 
+var a = 5+10;
+document.write(a);
 
+var b = 55-28;
+document.write(b);
 
+var c =10*2;
+document.write(c);
 
+var d = "3+6";
+document.write(d);
 
+var e ="2+3";
+var f ="4-1";
+document.write(e);
+document.write("<br>");
+document.write(f);
 
+var g = 5*5;
+var h ="5*5";
+document.write(h + "=" + g);
 
+var abc1 = 10%3;
+document.write(abc1);
 
+var abc2 = 5%2;
+document.write("5÷2の余りは"+ abc2);
+
+var abc3 ="8%2";
+document.write(abc3);
+
+var abc4 = 3%2;
+var abc5 = 1;
+document.write(abc4 + "=" + abc5);
