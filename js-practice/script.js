471101a213004e8cdff4fa360e0728b6ef856f6c
uswitch-02 <chunxidubian64@gmail.com>
2024-01-06 08:24:06 +0000

    [update]

---
+++ js-practice/script.js
@@ -0,0 +1,104 @@
+document.write("問1");
+document.write("<br>");
+
+for(var i=1; i<=5; i++){
+  document.write("★");
+}
+
+document.write("<br>");
+document.write("問2");
+for(var star1 = 1; star1 <=2; star1++){
+  document.write("<br>");
+  for(var star2 = 1; star2 <=3; star2++){
+    document.write("★");
+  }
+}
+
+
+document.write("<br>");
+document.write("問3");
+
+for(var star1 =1; star1 <= 2; star1++){
+  document.write("<br>");
+  for(var star2= 1; star2 <=5; star2++){
+    document.write("☆");
+    }
+}
+
+
+document.write("<br>");
+document.write("問4");
+document.write("<br>");
+
+
+for(var star1=1; star1<=5; star1++){
+  for(var star2=1; star2<=5; star2++){
+    document.write("★");
+  }
+  document.write("<br>");
+}
+
+document.write("問5");
+for(var star1 = 1; star1 <=4; star1++){
+  document.write("<br>");
+  for(var star2 = 1; star2 <=3; star2++){
+    document.write("★");
+  }
+}
+
+document.write("<br>");
+document.write("問6");
+for(var star1 = 1; star1 <=3; star1++){
+  document.write("<br>");
+  for(var star2 = 1; star2 <=1; star2++){
+    document.write("★");
+  }
+  for(var star3 = 1; star3 <=1; star3++){
+    document.write("☆");
+  }
+    for(var star2 = 1; star2 <=1; star2++){
+    document.write("★");
+  }
+}
+
+document.write("<br>");
+document.write("問7");
+for(var star1 = 1; star1 <=4; star1++){
+  document.write("<br>");
+  for(var star2 = 1; star2 <=1; star2++){
+    document.write("★");
+  }
+  for(var star3 = 1; star3 <=1; star3++){
+    document.write("☆");
+  }
+    for(var star4 = 1; star4 <=1; star4++){
+    document.write("★");
+  }
+    for(var star5 = 1; star5 <=1; star5++){
+    document.write("☆");
+  }
+    for(var star6 = 1; star6 <=1; star6++){
+    document.write("★");
+  }
+}
+
+// ↑絶対もっと簡単な記述できる
+document.write("<br>");
+document.write("問8");
+document.write("<br>");
+
+for(var star1 = 1; star1 <= 1; star1++){
+  document.write("★")
+}
+
+
+
+
+
+
+
+
+
+
+
+
