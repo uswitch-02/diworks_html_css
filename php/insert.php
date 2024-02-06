4e7f9425db9c20b05098c3c744a4175568412080
uswitch-02 <chunxidubian64@gmail.com>
2024-02-06 05:35:03 +0000

    だいぶいいぞ演習１

--- php/insert.php
+++
@@ -1,5 +0,0 @@
-<? php
-mb_internal_encoding("utf-8");
-PDO("mysql:dbname = lesson1; host = localhost;","root","");
-$pdo -> exec("insert into contactform(name,mail,age,comments)
-values('".$_POST['name']."','".$_POST['mail']."','".$_POST['age']."','".$_POST['comments']."');");
